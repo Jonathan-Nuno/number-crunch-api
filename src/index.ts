e6086c4ad7efe74931c42ae75ea952094da4e5ac
Jonathan-Nuno <70545522+Jonathan-Nuno@users.noreply.github.com>
2023-09-04 21:13:29 -0400

    Added cors
    Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors"
 import debtRoutes from "./routes/debtRoutes"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use(debtRoutes);
 
